b4f9f8a98ca0550c9fd6e6e3bf1dbc4fb38a6f35
Hacker <93960319+ayaan115@users.noreply.github.com>
2023-04-17 16:49:32 +0530

    Add files via upload

---
+++ screens/StarMap.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Text, View, TextInput } from 'react-native';
+import { WebView} from 'react-native-webview';
+
+const { longnitute, latitute } = this.state;
+const path = 'https://virtualsky.lco.global/embed/index.html?longitude=${longnitute}latitude=${latitute}&constellations=true&constellationlabels=true&showstarlabels=true&gridlines_az=true&live=true'
+
+export default class StarMapScreen extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            latitute: "",
+            longnitute: "",
+        }
+    }
+    render() {
+        return (
+            <TextInput 
+            style={{height: 40, borderColor: 'gray', borderWidth: 1,}}
+            placeholder='Enter Your Latitute'
+            placeholderTextColor= "#ffff#000000"
+            onChangeText={(text)=>{
+                this.setState({
+                    latitute: text
+                })
+            }}
+            />
+        )
+    }
+}
